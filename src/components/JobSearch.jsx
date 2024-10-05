44998ba7aa85c23033afb894ad8eb9b13704e84b
vivekbisen04 <121126140+vivekbisen04@users.noreply.github.com>
2024-10-05 17:40:27 +0530

    Dashboard

--- src/components/JobSearch.jsx
+++ src/components/JobSearch.jsx
@@ -7,12 +7,15 @@ import { toast } from "react-toastify";
 import CompanyCard from "./CompanyCard";
 import SearchBar from "./SearchBar";
 import "./Dashboard.css";
+import { urls } from "../urls";
 
 const JobSearch = () => {
   const yy = "http://localhost:4000"; // Fixed URL by removing extra space
 
   // State to store company data
   const [companies, setCompanies] = useState([]);
+  
+  
 
   // State for job applications and filters (if needed)
   const [jobApplications, setJobApplications] = useState([]);
